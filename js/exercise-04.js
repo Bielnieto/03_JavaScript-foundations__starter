20fc6d22da1e275c5be3c3cc7f8b958d41e9bcdb
Biel <biel.nieto@mataro.epiaedu.cat>
2024-11-18 17:41:09 +0100

    exercice-04 solved with the solution of the teacher

--- js/exercise-04.js
+++ js/exercise-04.js
@@ -8,3 +8,15 @@
 
     Once you see the results, write in a comment why it works that way
 */
+
+// "moo" is logged because the first operand is true, so the second operand is evaluated.
+true && console.log("moo");
+
+// Nothing is logged because the first operand is false, so the second operand is not evaluated.
+false && console.log("moo moo?");
+
+// Nothing is logged because the first operand is true, so the second operand is not evaluated.
+true || console.log("hello friend");
+
+// "bye friend" is logged because the first operand is false, so the second operand is evaluated.
+false || console.log("bye friend");
